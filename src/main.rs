fdc0ffa59a579790ef64e02e3c3e0a0fa251d542
Rajasekharan Vengalil <rajave@microsoft.com>
2022-08-14 17:49:16 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use std::{env, fmt::Display};
+
+use anyhow::Result;
+use futures_util::future::join_all;
+use reqwest::{Client, Url};
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Domain {
+    #[serde(rename = "Rank")]
+    rank: usize,
+
+    #[serde(rename = "Domain")]
+    domain: String,
+
+    #[serde(rename = "Open Page Rank")]
+    page_rank: f64,
+}
+
+impl Display for Domain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Domain: {}, Rank = {}, Open Page Rank = {}",
+            self.domain, self.rank, self.page_rank
+        )
+    }
+}
+
+const WORKERS: usize = 50;
+const CHANNEL_BUFFER: usize = 10;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut channels = vec![];
+    channels.resize_with(WORKERS, || mpsc::channel::<Domain>(CHANNEL_BUFFER));
+    let workers = channels
+        .into_iter()
+        .enumerate()
+        .map(|(index, (tx, rx))| (tx, tokio::spawn(worker_http(index, rx))))
+        .collect::<Vec<_>>();
+
+    if let Some(file_name) = env::args().nth(1) {
+        let mut rdr = csv::Reader::from_path(file_name)?;
+
+        for (index, result) in rdr.deserialize().enumerate() {
+            let (tx, _) = &workers[index % WORKERS];
+            tx.send(result?).await?;
+        }
+    } else {
+        usage();
+    }
+
+    // wait for the workers to do their thing
+    let _ = join_all(workers.into_iter().map(|(_, w)| w)).await;
+
+    Ok(())
+}
+
+fn usage() {
+    eprintln!("Usage:\n  check-domains <<domains.csv>>");
+}
+
+async fn worker_http(index: usize, mut rx: mpsc::Receiver<Domain>) -> Result<()> {
+    let client = Client::new();
+
+    while let Some(domain) = rx.recv().await {
+        let url = Url::parse(&format!("https://{}", domain.domain))?;
+        match client.head(url).send().await.map_err(|e| e.to_string()) {
+            Ok(res) => println!(
+                "[{index}] checked {}, status code: {}",
+                domain,
+                res.status()
+            ),
+            Err(err) if err.contains("EOF") => eprintln!("[{index}] BLOCKED! domain {}", domain),
+            Err(err) => eprintln!("[{index}] ERR! domain {}, {}", domain, err),
+        }
+    }
+
+    Ok(())
+}
